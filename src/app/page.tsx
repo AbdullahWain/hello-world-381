de4642006ef0d325bd63b9ed61775b46513e97ad
abwain381 <abdullahwain381@gmail.com>
2024-03-31 08:33:36 +0500

    Added Login , About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,7 @@ export default function Home() {
           priority
         />
       </div>
-      This is Abdullah and i am Busniess Development Executive at DigiFinders.
-
+                Hi,This is Abdullah Wain and i am Business Development Executive at BrandixSoft 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
